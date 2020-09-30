88ab23914c9046c719cab199fc993e961244475a
pedroaugustoferreira <43154432+pedroaugustoferreira@users.noreply.github.com>
2020-09-30 18:46:36 -0300

    Update install-zabbix-ibm-power-aix6.1.sh

--- install-zabbix-ibm-power-aix6.1.sh
+++ install-zabbix-ibm-power-aix6.1.sh
@@ -1,1 +1,70 @@
 
+#!/bin/ksh
+
+  
+# curl -s https://raw.githubusercontent.com/pedroaugustoferreira/service-amazonas-/master/install-zabbix-ibm-power-aix7.2.sh | bash -s --
+
+rm -rf /tmp/zabbix_install
+mkdir /tmp/zabbix_install
+cd /tmp/zabbix_install
+
+ps -ef|grep [z]abbix_agentd|awk '{print "kill -9 "$2}'|sh
+
+wget https://www.zabbix.com/downloads/1.8.3/zabbix_agent-1.8.3-aix-6.1.04-powerpc.tar.gz
+
+gzip -d zabbix_agent-1.8.3-aix-6.1.04-powerpc.tar.gz
+
+tar -xf zabbix_agent-1.8.3-aix-6.1.04-powerpc.tar
+
+#create zabbix group
+mkgroup zabbix
+
+#create zabbix user and at it to zabbix group
+mkuser pgrp='zabbix' groups='zabbix' zabbix
+
+#make configuration directory
+mkdir /etc/zabbix
+
+#make log directory
+mkdir /var/log/zabbix/
+
+#log directory belongs to user zabbix and group zabbix
+chown -R zabbix:zabbix /var/log/zabbix/
+
+#make configuration directory
+mkdir -p /usr/local/etc/
+
+#create direoctory for zabbix process file
+mkdir -p /var/run/zabbix/
+
+#process files directory belong to zabbix
+chown -R zabbix:zabbix /var/run/zabbix/
+
+#install zabbix_get and zabbix_sender binary 
+cp bin/zabbix_* /bin/
+
+#install zabbix_agent and zabbix_agentd binary
+cp sbin/zabbix_agent* /sbin/
+
+#copy configuration files
+cp -r conf/zabbix_agent* /etc/zabbix/
+
+#link configiguration file
+ln -s /etc/zabbix/zabbix_agentd.conf /usr/local/etc/zabbix_agentd.conf
+
+#backup original configuration file
+mv /etc/zabbix/zabbix_agentd.conf /etc/zabbix/zabbix_agentd.conf_bck
+
+#create your own configuration file
+echo "PidFile=/var/run/zabbix/zabbix_agentd.pid" > /etc/zabbix/zabbix_agentd.conf
+echo "LogFile=/var/log/zabbix/zabbix_agentd.log" >> /etc/zabbix/zabbix_agentd.conf
+echo "EnableRemoteCommands=1" >> /etc/zabbix/zabbix_agentd.conf
+echo "LogRemoteCommands=1" >> /etc/zabbix/zabbix_agentd.conf
+echo "Server=10.1.8.101" >> /etc/zabbix/zabbix_agentd.conf
+echo "ServerActive=10.1.8.101" >> /etc/zabbix/zabbix_agentd.conf
+echo "Hostname=$(hostname -s)" >> /etc/zabbix/zabbix_agentd.conf
+
+#start zabbix agent
+zabbix_agentd
+
+mkitab "zabbix:2:once:/sbin/zabbix_agentd >/dev/null 2>&1"
